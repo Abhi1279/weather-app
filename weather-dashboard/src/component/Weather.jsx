5caa73f54910d65e67486564bc6b09d9652df2d7
Abhi1279 <achougule1127@gmail.com>
2024-09-28 13:34:11 +0530

    Implemented Card Design

--- weather-dashboard/src/component/Weather.jsx
+++ weather-dashboard/src/component/Weather.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+
 const Weather = () => {
+    
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu","Fri"];
+
   return (
     <div className="container-fluid">
       <div className="row" style={{ backgroundColor: "#eeeee4" }}>
@@ -9,7 +13,6 @@ const Weather = () => {
         </div>
         <div className="col-12 col-md-4"></div>
       </div>
-
       <div className="row">
         <div
           className="col-12 col-md-3"
@@ -32,16 +35,23 @@ const Weather = () => {
             </button>
           </div>
         </div>
-
         <div
           className="col-12 col-md-9"
-          style={{ backgroundColor: "#abdbe3", paddingLeft: "55px",paddingRight:"55px",paddingTop:"15px"}}
+          style={{
+            backgroundColor: "#abdbe3",
+            paddingLeft: "55px",
+            paddingRight: "55px",
+            paddingTop: "15px",
+          }}
         >
           <div className="row">
             <div className="col-12 col-md-4">
-            <div className="d-flex justify-content-start">
-                <h4 style={{borderBottom:"1px solid"}}>Upcoming Week's Forecast</h4>
-            </div></div>
+              <div className="d-flex justify-content-start">
+                <h4 style={{ borderBottom: "1px solid" }}>
+                  Upcoming Week's Forecast
+                </h4>
+              </div>
+            </div>
             <div className="col-12 col-md-4"></div>
             <div className="col-12 col-md-4">
               <div className="d-flex justify-content-end" style={{ gap: "20px" }}>
@@ -68,15 +78,17 @@ const Weather = () => {
               </div>
             </div>
           </div>
-          <div className="row">
-            <div className="col-md-2" style={{paddingTop:"60px"}}>
-            <div className="card" style={{height:"150px",width:"180px",padding:"10px"}}>
-                  <h5>Sun</h5>
-                  <img src="" alt="no_image" />
 
-                  <h5>15° 3°</h5>
+          <div className="row" style={{paddingTop:"35px"}}>
+          {days.map((day) => (
+            <div key={day} className="col-12 col-md-6 col-lg-2 d-flex justify-content-center mb-4">
+              <div className="card" style={{ width: "180px", padding: "10px" }}>
+                <h5>{day}</h5>
+                <img src="" alt="no_image" style={{ height: "50px", width: "100%" }} />
+                <h5>15° 3°</h5>
+              </div>
             </div>
-            </div>
+          ))}
           </div>
         </div>
       </div>
